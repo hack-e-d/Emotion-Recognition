8e6054a2f2ff0bb6b90d6dbc50bb21cf11ab651a
Vijay P V <vijayraj1998@yahoo.com>
2019-12-12 18:37:26 +0530

    Add files via upload

--- play_sound.py
+++ play_sound.py
@@ -1,2 +1,5 @@
 from playsound import playsound
-playsound('C:/Users/Administrator/Downloads/Darbar-Motion-Teaser-BGM-Ringtone.mp3')
+def playSong(mood):
+    if(mood in [3,4,6]):
+        path={3 : "G:/Documents/Machine_Learning/Emotion-Recognition-song-recomendation-master/music/happy/1.mp3",4 : "G:/Documents/Machine_Learning/Emotion-Recognition-song-recomendation-master/music/sad/1.mp3",6 : "G:/Documents/Machine_Learning/Emotion-Recognition-song-recomendation-master/music/neutral/1.mp3"}
+        playsound(path[mood])
